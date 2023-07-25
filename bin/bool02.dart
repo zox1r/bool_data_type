c940a9f7ac1269bcb89b73ae5fbd1da92591556c
zox1r <qarshiyevzoxir@gmail.com>
2023-07-25 10:28:18 +0200

    bin/bool02.dart

--- bin/bool02.dart
+++ bin/bool02.dart
@@ -16,7 +16,9 @@ bool func(int a) {
     Returns:
       A boolean value indicating whether a is equal to b.
   */
-  return false;
+  return a == 7;
 }
 
-void main() {}
+void main() {
+  print(func(7));
+}
